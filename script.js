f72d109ae935478fb783435e00fd6526f34fa9f3
GrahamLearnsCode <grahamlearnscode@gmail.com>
2020-03-26 15:51:41 +0000

    add toggling of buttons on click 'play'. Refactor play journey into more distinct functions. Add first pass of dom updating messaging. Disable 5-round play.

--- script.js
+++ script.js
@@ -1,96 +1,77 @@
-////Working fine on valid inputs. Invalid inputs are logged, haven't yet found a way to loop back to the correct starting point of calling the function though
+//returning winner still needs to happen for round counters to work
+//but winnerText should not use winner. Let's take the console log lines and have those be winnerText - they're already written to read correctly whether win/lose/draw
+//Also need to give transparency in the DOM - you chose x computer chose y result was z
 
-let w=0;
-let c=0;
-let i=0;
+function togglePlayControls() {
+    const playButton = document.querySelector('#play');
+    playButton.classList.toggle('inactive');
+    let selectButtons = document.querySelectorAll('.select');
+    selectButtons.forEach(foo => foo.classList.toggle('select'));
+}
+
+function playRound(playerChoice) {
+    console.log('Player chose ' + playerChoice);
+    let computerChoice=computerPlay();
+    console.log('Computer chose ' + computerChoice);
+    let winner=compareChoices(playerChoice, computerChoice);
+    console.log('winner is ' + winner);
+    updateWinnerText(winner);
+}
+
+function compareChoices(playerChoice, computerChoice) {
+    console.log('comparing ' + playerChoice + ' to ' + computerChoice);
+    switch (playerChoice) {
+        case computerChoice:
+        console.log('You both chose ' + playerChoice + '. That\'s a draw!');
+        winner='draw';
+        break;
+    
+        case 'Rock':
+            if (computerChoice==='Scissors') {console.log('Rock blunts Scissors. You win!'); winner='player';}
+            else {console.log('Paper covers Rock. You lose!'); winner='computer';};
+        break;
+    
+        case 'Paper':
+            if (computerChoice==='Rock') {console.log('Paper covers Rock. You win!'); winner='player';}
+            else {console.log('Scissors cuts Paper. You lose!'); winner='computer';};
+        break;
+    
+        case 'Scissors':
+            if (computerChoice==='Paper') {console.log('Scissors cuts Paper. You win!'); winner='player';}
+            else {console.log('Rock blunts Scissors. You lose!'); winner='computer';};
+        break;
+        
+        default: console.error('Invalid comparison: ' + playerChoice + ' + ' + computerChoice);}
+        return winner;
+   }
+
+   function updateWinnerText(winner) {
+    console.log('updating winner text to show ' + winner);
+    let winnerText = document.querySelector('#winnerText');
+    winnerText.textContent='A winner is ' + winner;
+}
 
 function computerPlay() {
-
     //choose a random number 1-3
     function randomInteger (max) { 
         let myRandomInt = (Math.floor(Math.random()*max)+1);
-    /*console.log('myRandomInt is ' + myRandomInt);*/
-    return myRandomInt;
-    }
-
+        return myRandomInt;
+        }
     let computerChoice = randomInteger(3);
-    /*console.log(computerChoice);*/
-
     switch(computerChoice) {
         case 1: computerChoice = 'Rock'; break;
         case 2: computerChoice = 'Paper'; break;
         case 3: computerChoice = 'Scissors'; break;
         default: console.error('Error: computer didn\'t choose anything');
-}
-
-/*console.log('Computer chose ' + computerChoice);*/
-return computerChoice;
-}
-
-function playerSelection() {
-let playerChoice = prompt('Make your choice: Rock, Paper or Scissors');
-//Handle casing, convert to Proper Case
-    playerChoice=playerChoice.toLowerCase();
-    playerChoice=playerChoice.replace(playerChoice.charAt(0), playerChoice.charAt(0).toUpperCase());
-//Handle invalid inputs
-    switch(playerChoice) {
-        case 'Rock': break;
-        case 'Paper': break;
-        case 'Scissors': break;
-        
-        //Handle 'paper scissors stone' people
-        case 'Stone': 
-            alert('Um...I\'m going to assume you meant \'Rock\' there...'); 
-            playerChoice='Rock';
-            break;
-        
-        //Handle Konami code
-        case 'Uuddlrlrba':alert('God mode activated...');alert('...it\'s the same as regular mode, but you can feel proud of yourself for trying'); playerChoice=null; playerChoice=playerSelection(); break;
-        
-        default: console.error('Invalid input: ' + playerChoice); 
-        alert('That wasn\'t a valid input, try again');
-        playerChoice=null;
-    }
-
-/*console.log('Within-function: player chose ' + playerChoice);*/
-return playerChoice;
+        }
+    return computerChoice;
 }
 
 
-function playRound() {
-let playerChoice=playerSelection();
-console.log('Player chose ' + playerChoice);
-let computerChoice=computerPlay();
-console.log('Computer chose ' + computerChoice);
-let winner
-//Compare for who wins
-switch (playerChoice) {
-    case computerChoice:
-    console.log('You both chose ' + playerChoice + '. That\'s a draw!');
-    winner='draw';
-    break;
+//Counters for round winners
+let w=0; let c=0; let i=0;
 
-    case 'Rock':
-        if (computerChoice==='Scissors') {console.log('Rock blunts Scissors. You win!'); winner='player';}
-        else {console.log('Paper covers Rock. You lose!'); winner='computer';};
-    break;
-
-    case 'Paper':
-        if (computerChoice==='Rock') {console.log('Paper covers Rock. You win!'); winner='player';}
-        else {console.log('Scissors cuts Paper. You lose!'); winner='computer';};
-    break;
-
-    case 'Scissors':
-        if (computerChoice==='Paper') {console.log('Scissors cuts Paper. You win!'); winner='player';}
-        else {console.log('Rock blunts Scissors. You lose!'); winner='computer';};
-    break;
-    
-    default: console.error('Invalid comparison: ' + playerChoice + ' + ' + computerChoice);
-}
-return winner;}
-
-
-function game() {
+function fiveRoundGame() {
     if (i<5) {
         let winner=playRound();
         i++;
@@ -111,4 +92,7 @@ function game() {
         else {console.log('It was a draw.')}
         }
 }
+
+
+
 
